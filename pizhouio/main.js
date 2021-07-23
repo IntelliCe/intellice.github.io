ca6e321570e952f93629d731767b66de05ebb2e7
cSquared <49149803+IntelliCe@users.noreply.github.com>
2021-07-23 20:37:58 +0800

    Update main.js

--- pizhouio/main.js
+++ pizhouio/main.js
@@ -1,4 +1,4 @@
 function onDownload() {
     //alert("The download is not yet open, please continue to pay attention!")
-    window.location.href = 'https://github.com/IntelliCe/PizhouIOApp/releases/download/2.2.0-beta/pizhou-io-2.2.0-beta.apk'
+    window.location.href = 'https://github.com/IntelliCe/PizhouIOApp/releases/download/2.2.1-beta/pizhou-io-2.2.1-beta.apk'
 }
